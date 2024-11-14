509e7527ef0bf2c10c74f278dfc34ff087b79f36
HeyJezs <jessikasanchezc@gmail.com>
2024-11-14 02:53:41 -0500

    estructura basica de microservicios

---
+++ AccountMs/src/test/java/com/banking/AccountMs/AccountMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.AccountMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
